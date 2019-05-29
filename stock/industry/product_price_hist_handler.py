40d89ba9ce8481332212c5744c3f57e6de1efe44
taotao <liuhuiyu@qiandongdong.cn>
2019-05-29 13:34:40 +0800

    bugfixed

--- stock/industry/product_price_hist_handler.py
+++ stock/industry/product_price_hist_handler.py
@@ -20,20 +20,30 @@ def get_grouped_category1(category0, dateform, dateto):
 
 
 def get_category0_list(category0, dateform, dateto):
-    records = session.query(ProductHistPrice).filter(and_(ProductHistPrice.category0 == category0,
-                                                          ProductHistPrice.date >= dateform,
-                                                          ProductHistPrice.date <= dateto,
-                                                          )).order_by(ProductHistPrice.category1, ProductHistPrice.date).all()
-    return records
+    try:
+        records = session.query(ProductHistPrice).filter(and_(ProductHistPrice.category0 == category0,
+                                                              ProductHistPrice.date >= dateform,
+                                                              ProductHistPrice.date <= dateto,
+                                                              )).order_by(ProductHistPrice.category1, ProductHistPrice.date).all()
+        return records
+    except Exception as e:
+        pass
+    finally:
+        session.close()
 
 
 def get_category1_list(category0, category1, dateform, dateto):
-    records = session.query(ProductHistPrice).filter(and_(ProductHistPrice.category0 == category0,
-                                                          ProductHistPrice.category1 == category1,
-                                                          ProductHistPrice.date >= dateform,
-                                                          ProductHistPrice.date <= dateto,
-                                                          )).all()
-    return records
+    try:
+        records = session.query(ProductHistPrice).filter(and_(ProductHistPrice.category0 == category0,
+                                                              ProductHistPrice.category1 == category1,
+                                                              ProductHistPrice.date >= dateform,
+                                                              ProductHistPrice.date <= dateto,
+                                                              )).all()
+        return records
+    except Exception as e:
+        pass
+    finally:
+        session.close()
 
 
 if __name__ == '__main__':
